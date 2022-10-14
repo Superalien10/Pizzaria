a183f28721afdfd5472d0c51778c1ab8c865f271
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-10-14 09:20:53 -0300

    Update teste.py
    Teste

--- testes/teste.py
+++ testes/teste.py
@@ -1,8 +1,5 @@
 import sys
 
-sys.path.insert(0, "../src")
-
-import modulo_qualidade as mq
 
 import pizzza
 import unittest
@@ -28,10 +25,9 @@ class TestModuloQualidade(unittest.TestCase):
         print('\nExecutando tearDownMethod')
     def test_case_pizza_calabresa(self):
         print('Teste de montar pizza de calabresa')
-    
     def test_case_comum(self):
         print("Executando Caso de Teste: Comum")
-        self.assertEqual( remover_ingrediente("cobertura", "pepperoni"), {'massa': {'trigo': 19.34}, 'molho': {'tomate': 5}, 'cobertura': {}, 'queijo': {'gorgonzola': 2}})
+        self.assertEqual( pizzza.remover_ingrediente("cobertura", "pepperoni"), {'massa': {'trigo': 19.34}, 'molho': {'tomate': 5}, 'cobertura': {}, 'queijo': {'gorgonzola': 2}})
         
 if __name__ == '__main__':
     unittest.main(verbosity=2)
