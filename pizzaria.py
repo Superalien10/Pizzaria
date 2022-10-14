c0a0839f46efa34a16083b300218282e748a458b
GeorgeRV <48179995+GeorgeRV@users.noreply.github.com>
2022-10-14 08:18:38 -0300

    Update pizzaria.py

--- pizzaria.py
+++ pizzaria.py
@@ -1,3 +1,11 @@
+def list_ingredientes(dict_ingredientes: dict[str, dict[str, float]], list_tipos: list[str] = ["massa", "molho", "queijo", "cobertura"]):
+    """recebe um dicionario de ingredientes e imprime os ingredientes e os seus preços
+
+    Args:
+        dict_ingredientes (dict[str, dict[str, float]]): _description_
+        list_tipos (list[str], optional): lista dos tipos de ingredientes a serem listados. Deve ser do tipo "massa", "molho", "queijo" ou "cobertura". Defaults to ["massa", "molho", "queijo", "cobertura"].
+    """
+
 def remover_ingrediente(ingrediente: str, dict_ingredientes: dict)-> dict:
     """Remove um ingrediente dicionário de ingredientes cadastrados.
 
