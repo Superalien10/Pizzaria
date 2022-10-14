b9c94c0fdd8930ce89c4b2f8a21ad9b246778365
Vinícius Sousa <54545608+ViniAS@users.noreply.github.com>
2022-10-14 08:21:15 -0300

    Update pizzaria.py

--- pizzaria.py
+++ pizzaria.py
@@ -2,17 +2,43 @@ def list_ingredientes(dict_ingredientes: dict[str, dict[str, float]], list_tipos
     """recebe um dicionario de ingredientes e imprime os ingredientes e os seus preços
 
     Args:
-        dict_ingredientes (dict[str, dict[str, float]]): _description_
+        dict_ingredientes (dict[str, dict[str, float]]): Um dicionario que tem como chave
+        o tipo do ingrediente e como item dicionário que contém como chave o nome do ingrediente e como item o preço
         list_tipos (list[str], optional): lista dos tipos de ingredientes a serem listados. Deve ser do tipo "massa", "molho", "queijo" ou "cobertura". Defaults to ["massa", "molho", "queijo", "cobertura"].
     """
 
-def remover_ingrediente(ingrediente: str, dict_ingredientes: dict)-> dict:
+def remover_ingrediente(ingrediente: str, dict_ingredientes: dict[str, dict[str,float]])-> dict[str, dict[str,float]]:
     """Remove um ingrediente dicionário de ingredientes cadastrados.
 
     Args:
-        ingrediente (str): _description_
-        dict_ingredientes (dict): _description_
+        ingrediente (str): nome do ingrediente a ser adicionado.
+        dict_ingredientes (dict[str, dict[str,float]]): Um dicionario que tem como chave
+        o tipo do ingrediente e como item dicionário que contém como chave o nome do ingrediente e como item o preço.
 
     Returns:
-        dict: _description_
+        dict[str, dict[str,float]]: dicionario com o ingrediente removido
+    """
+def add_ingrediente(ingrediente: str, preco: float, tipo: str, dict_ingredientes: dict[str, dict[str,float]] = None) -> dict[str, dict[str,float]]:
+    """recebe um dicionário de ingredientes e adiciona um novo ingrediente de um determinado tipo. Imprime o ingrediente, o tipo e o preço
+
+    Args:
+        ingrediente (str): nome do ingrediente a ser adicionado. Caso o ingrediente já exista, o preço é atualizado
+        preco (float): preço do ingrediente.
+        tipo (str): tipo do ingrediente. Deve ser 'massa', 'molho', 'queijo' ou 'cobertura', caso não seja um desses valores, não será adicionado
+        dict_ingredientes (dict[str, dict[str,float]], optional):  Um dicionario que tem como chave
+        o tipo do ingrediente e como item dicionário que contém como chave o nome do ingrediente e como item o preço
+
+    Returns:
+        dict[str, dict[str,float]]: dicionário com o novo ingrediente adicionado
+    """
+
+def montar_pizza(dict_ingredientes: dict[str, dict[str,float]]) -> float:
+    """Recebe um dicionário de ingredientes e calcula o preço da pizza. Imprime o dicionário de ingredientes e o valor da pizza
+
+    Args:
+        dict_ingredientes (dict[str, dict[str,float]]): Um dicionario que tem como chave
+        o tipo do ingrediente e como item dicionário que contém como chave o nome do ingrediente e como item o preço
+
+    Returns:
+        float: preço da pizza
     """    
