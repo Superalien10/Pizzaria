fe677458f991b1e6d3c20cc6efc4ee5a31f738ea
Vinícius Sousa <54545608+ViniAS@users.noreply.github.com>
2022-10-14 09:03:51 -0300

    Update pizzza.py

--- testes/pizzza.py
+++ testes/pizzza.py
@@ -7,8 +7,20 @@ def cadastrar_ingrediente(tipo, ingrediente, valor):
     :type ingrediemte: string
     :param valor: valor do ingrediente
     :type valor: float
+    :return: dicionário com os ingredientes
+    :return type: dict
+    >>> cadastrar_ingrediente("massa", "trigo", 19.34)
+    {'massa': {'trigo': 19.34}, 'molho': {}, 'cobertura': {}, 'queijo': {}}
+    >>> cadastrar_ingrediente("molho", "tomate", 5)
+    {'massa': {'trigo': 19.34}, 'molho': {'tomate': 5}, 'cobertura': {}, 'queijo': {}}
+    >>> cadastrar_ingrediente("cobertura", "pepperoni", 5)
+    {'massa': {'trigo': 19.34}, 'molho': {'tomate': 5}, 'cobertura': {'pepperoni': 5}, 'queijo': {}}
+    >>> cadastrar_ingrediente("queijo", "gorgonzola", 2)
+    {'massa': {'trigo': 19.34}, 'molho': {'tomate': 5}, 'cobertura': {'pepperoni': 5}, 'queijo': {'gorgonzola': 2}}
     """
-    pass
+    ingredientes[tipo][ingrediente]=valor
+    return ingredientes
+
 
 def montar_pizzza_valor(massa, molho, queijo, cobertura):
     """Montar pizza e retornar valor
@@ -22,8 +34,17 @@ def montar_pizzza_valor(massa, molho, queijo, cobertura):
     :type cobertura: string
     :return: preço da pizza
     :return type: float
+    
+    >>> montar_pizzza_valor("trigo", "tomate", "gorgonzola", "pepperoni")
+    31.34
     """
-    pass
+    valor = 0
+    valor+=ingredientes["massa"][massa]
+    valor+=ingredientes["molho"][molho]
+    valor+=ingredientes["queijo"][queijo]
+    valor+=ingredientes["cobertura"][cobertura]
+
+    return valor
 
 def remover_ingrediente(tipo,ingrediente):
     """Remove ingrediente do inventário
@@ -31,8 +52,13 @@ def remover_ingrediente(tipo,ingrediente):
     :type tipo: string
     :param ingrediente: ingrediente
     :type ingrediente: string
+    :return: dicionário com os ingredientes
+    :return type: dict
+    >>> remover_ingrediente("cobertura", "pepperoni")
+    {'massa': {'trigo': 19.34}, 'molho': {'tomate': 5}, 'cobertura': {}, 'queijo': {'gorgonzola': 2}}
     """
-    pass
+    ingredientes[tipo].pop(ingrediente)
+    return ingredientes
 
 def listar_ingredientes(tipo):
     """Lista ingredientes e seus preços
@@ -40,5 +66,13 @@ def listar_ingredientes(tipo):
     :type tipo: string
     :return: dicionário dos ingredientes desse tipo e seus respectivos preços
     :return type: dict
+    >>> listar_ingredientes("massa")
+    {'trigo': 19.34}
     """
-    pass
+    tipo_ingrediente=ingredientes[tipo]
+    return tipo_ingrediente
+
+
+if __name__ =="__main__":
+    import doctest
+    doctest.testmod(verbose=True)
