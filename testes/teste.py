a4a45141c5ab5a62d3661c1b09fa2e144bfbc3e2
Vinícius Sousa <vinicius.sousa@fgv.edu.br>
2022-10-14 08:54:37 -0300

    Update teste.py

--- testes/teste.py
+++ testes/teste.py
@@ -1,8 +1,27 @@
 import sys
 
-sys.path.insert(0,'../src')
-
 import contrato
 import unittest
 from datetime import date
 from unittest.mock import Mock
+
+def setUpModule():
+    print('\nExecutando SetUpModule')
+
+def tearDownModule():
+    print('\nExecutando tearDownModule')
+
+class TestModuloQualidade(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print('\nExecutando setUpClass')
+    @classmethod
+    def tearDownClass(cls):
+        print('\nexecutando tearDownClass')
+    def setUp(self):
+        print('\nExecutando setUpClass')
+    def tearDown(self):
+        print('\nExecutando tearDownMethod')
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
