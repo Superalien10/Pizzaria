024aaa3e9d5e085a69cbbff449505ec2ab2f6abc
Vinícius Sousa <54545608+ViniAS@users.noreply.github.com>
2022-10-14 09:22:11 -0300

    Update teste.py

--- testes/teste.py
+++ testes/teste.py
@@ -5,7 +5,7 @@ import pizzza
 import unittest
 from datetime import date
 from unittest.mock import Mock
-
+from mock import patch
 def setUpModule():
     print('\nExecutando SetUpModule')
 
@@ -28,6 +28,9 @@ class TestModuloQualidade(unittest.TestCase):
     def test_case_comum(self):
         print("Executando Caso de Teste: Comum")
         self.assertEqual( pizzza.remover_ingrediente("cobertura", "pepperoni"), {'massa': {'trigo': 19.34}, 'molho': {'tomate': 5}, 'cobertura': {}, 'queijo': {'gorgonzola': 2}})
-        
+    @patch('pizzza.ingredientes',{"massa":{'trigo':10}, "molho":{'tomate':10}, "cobertura":{'calabresa':10}, "queijo":{'mussarela':10}})
+    def test_case_pizza_calabresa(self):
+        print('Teste de montar pizza de calabresa')
+        self.assertEqual(pizzza.montar_pizzza_valor('trigo','tomate', 'mussarela',"calabresa"),40)   
 if __name__ == '__main__':
     unittest.main(verbosity=2)
