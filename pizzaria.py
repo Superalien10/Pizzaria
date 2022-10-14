3bbc6463eb3281d09394f4ea29f1dfc12d0ae1ec
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-10-14 08:13:58 -0300

    Create pizzaria.py

---
+++ pizzaria.py
@@ -0,0 +1,10 @@
+def remover_ingrediente(ingrediente: str, dict_ingredientes: dict)-> dict:
+    """Remove um ingrediente dicionário de ingredientes cadastrados.
+
+    Args:
+        ingrediente (str): _description_
+        dict_ingredientes (dict): _description_
+
+    Returns:
+        dict: _description_
+    """    
